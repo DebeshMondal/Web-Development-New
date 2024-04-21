4961558619915b906d275566850674b76437216e
Debesh Mondal <debeshprofessional05@gmail.com>
2024-04-21 11:21:48 +0530

    Array in Java Script
    Array  in java script.

---
+++ JAVA SCRIPT (ARRAY)/index.js
@@ -0,0 +1,11 @@
+let arr = [1,3,5,7,9]
+ // index  0,1,2,3,4
+
+console.log(arr);
+console.log(arr.length)
+console.log(arr[0])
+console.log(arr[2])
+console.log(arr[4])
+console.log(typeof(arr))
+console.log(arr.toString())
+console.log(arr.join(" and "))
